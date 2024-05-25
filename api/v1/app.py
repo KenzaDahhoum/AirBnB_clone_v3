a2fa586542d4f0920110e0ed082a6279cce04323
KenzaDahhoum <kenza.dahhoum@gmail.com>
2024-05-25 15:43:08 -0700

    app

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,10 +3,11 @@
 """
 
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -15,6 +16,12 @@ app.register_blueprint(app_views)
 def teardown_appcontext(exception):
     """Close the storage"""
     storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handler for 404 errors that returns a JSON-formatted 404 status code response"""
+    return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
